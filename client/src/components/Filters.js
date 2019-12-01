01fb207e83db801f8f8fe6af3f0b9ca1eddc5eba
mdanlowski <marcindanlowski@gmail.com>
2019-12-01 14:41:18 +0100

    continued frontend add components

---
+++ client/src/components/Filters.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+class Filters extends React.Component {
+  render(){
+    return(
+      <div className="filters">
+        jaja
+      </div>
+    )
+  }
+}
+
+
+export default Filters;
